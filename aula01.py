d6ad90d851cbcb3bfe8bf775a18b2e894257f2f8
Lavínia Ferreira <souzlaviniasouza.@gmail.com>
2025-08-20 12:57:13 -0300

    aula1

---
+++ aula01.py
@@ -0,0 +1,13 @@
+# Definindo variaveis
+nome = "Lavínia"
+idade = 15
+altura = 1.66
+situacaoTrabalho = True
+
+# Exibindo dados com a função print
+print("Hello World!")
+print('A soma é:',2+3 )
+print('A multiplicação: {}'. format(5*5))
+print(f'A divisão é {10/2}')
+
+print(f'Seu nome é {nome}, sua idade é {idade} e sua altura é {altura}') 
